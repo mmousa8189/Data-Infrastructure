6602fafb9fa12977ce8dcb88f91242c5c321e4e7
Mohamed Sayed Mousa <m.mousa8189@hotmail.com>
2016-04-01 16:06:59 +0200

    First work create IDataAC Interface

---
+++ DataInfrastructure/IDataAC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInfrastructure
+{
+   public interface IDataAC <TEntity>
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable GetAllWithIncludes(object id, params Expression<Func<TEntity, object>>[] includes);
+        TEntity GetById(object id);
+
+        TEntity GetByIdWithIncludes(object id, params Expression<Func<TEntity, object>>[] includes);
+        TEntity Insert(TEntity entity);
+        TEntity Update(TEntity entity,object id);
+        TEntity Update(TEntity entity, params object[] id);
+        void Delete(TEntity entity);
+        IQueryable<TEntity> GetCompiledQuery(Expression<Func<TEntity, bool>> predicate);
+            
+    }
+}
